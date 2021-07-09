a3e3046728313a5dfb3a62014480e26367502b8c
Robert <ro.behrensdeluna@gmail.com>
2021-07-09 14:24:48 +0200

    sphinx_rtd_theme added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,14 +48,11 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
-import sphinx_rtd_theme
 
-extensions = [
-    ...
-    'sphinx_rtd_theme',
-]
-
+#html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
